5c3c04f65d6dd5979222545b5526a20dfa6d2998
vijaypapineni <p.vijayakrishna@ymail.com>
2015-05-09 15:46:55 +0530

    Added angular kickstart and changes to todo app

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,17 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.todos = [
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Item 4'
     ];
+    $scope.addTodo = function () {
+    $scope.todos.push($scope.todo);
+    $scope.todo = '';
+    };
+    $scope.removeTodo = function (index) {
+		$scope.todos.splice(index, 1);
+    };
   });
